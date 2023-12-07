6185dcfe5101d3f82ff51c648110a2c46af79ce7
Alexandre <alexandresargitario@gmail.com>
2023-12-06 21:49:26 -0300

    Mudanças para hopendagem

--- PlayCauseApi/src/main.ts
+++ PlayCauseApi/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
-import { MySocketModule } from './app/socket/socket.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
+const port = process.env.PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(UsuariosModule);
@@ -13,25 +12,9 @@ async function bootstrap() {
         allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
       };
     app.enableCors(corsOptions);
-  await app.listen(3001);
+  await app.listen(port);
 
 }
 
 bootstrap();
 
-async function bootstrap2() {
-  const app = await NestFactory.create(MySocketModule);
-  const corsOptions: CorsOptions = {
-    origin: "*", // Habilita todos os origens
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-  };
-  const ioAdapter = new IoAdapter(app);
-  app.useWebSocketAdapter(ioAdapter);
-  app.enableCors(corsOptions);
-
-  await app.listen(3333);
-
-}
-
-bootstrap2();
